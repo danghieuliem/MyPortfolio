fb009ca336eefbd385ac9b7636f28e59fb1c4571
Liêm Đặng <60537515+danghieuliem@users.noreply.github.com>
2023-06-22 23:54:23 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	reactStrictMode: true,
+	basePath: basePath,
+
+	// Set the asset prefix for GitHub Pages
+	assetPrefix: `MyPortfolio/`,
+
+	// Enable webpack 5 for better build performance
+	future: {
+		webpack5: true,
+	},
+
+	// For GitHub Pages, set the trailing slash
+	trailingSlash: true,
 };
 
 module.exports = nextConfig;
