a0838993d7e90f30bae10a0fc6b8fd60513e7480
Liêm Đặng <60537515+danghieuliem@users.noreply.github.com>
2023-06-23 15:43:32 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
 	basePath: `/MyPortfolio`,
 
 	// Set the asset prefix for GitHub Pages
-	assetPrefix: `./`,
+	assetPrefix: ``,
 
 	// Enable webpack 5 for better build performance
 	future: {
